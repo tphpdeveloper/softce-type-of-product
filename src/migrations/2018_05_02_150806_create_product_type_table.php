3b563a7499099506767faff4d4f86f3d1651a818
Igor <ig@c-ua.com>
2018-06-07 10:28:09 +0300

    Add rules cascade to relation in migration file

--- src/migrations/2018_05_02_150806_create_product_type_table.php
+++ src/migrations/2018_05_02_150806_create_product_type_table.php
@@ -15,10 +15,10 @@ class CreateProductTypesTable extends Migration
     {
         Schema::create('product_type', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('type_id')->unsigned();
-            $table->foreign('type_id')->references('id')->on('types');
             $table->integer('product_id')->unsigned();
-            $table->foreign('product_id')->references('id')->on('products');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade')->onUpdate('cascade');
+            $table->integer('type_id')->unsigned();
+            $table->foreign('type_id')->references('id')->on('types')->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
         });
     }
