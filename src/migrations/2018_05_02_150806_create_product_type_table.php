a7cd243bead91d117c2df0309af9a164eda15f32
Igor <ig@c-ua.com>
2018-07-06 14:07:32 +0300

    Fix name class migration

--- src/migrations/2018_05_02_150806_create_product_type_table.php
+++ src/migrations/2018_05_02_150806_create_product_type_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProductTypesTable extends Migration
+class CreateProductTypeTable extends Migration
 {
     /**
      * Run the migrations.
